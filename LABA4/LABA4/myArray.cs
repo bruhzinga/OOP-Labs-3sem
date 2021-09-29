f6540c51966d1d12ef0a0fca758f62253084a9d8
bruhzinga <48861240+bruhzinga@users.noreply.github.com>
2021-09-30 00:55:20 +0300

    FOUNDATION

--- LABA4/LABA4/myArray.cs
+++ LABA4/LABA4/myArray.cs
@@ -8,9 +8,57 @@ namespace LABA4
 {
     internal class MyArray
     {
+        public class Owner
+        {
+            private string name;
+            private string organization;
+            private static int Id;
+
+            public Owner(string name, string organization)
+            {
+                this.name = name;
+                this.organization = organization;
+                Id++;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"ID: {Id}\n" +
+                    $"Name: {name}\n" +
+                    $"Organization: {organization}");
+            }
+        }
+
+        public class Date
+        {
+            private DateTime time;
+
+            public Date()
+            {
+                time = DateTime.Now;
+            }
+
+            public void ShowDate()
+            {
+                Console.WriteLine(time);
+            }
+        }
+
+        public Owner _Owner = new Owner("DIMA", "BSTU");
+        public Date _Date = new Date();
         private int[] Myarray_i { get; set; }
 
         private char[] Myarray_c { get; set; }
+
+        public MyArray(params int[] ints)
+        {
+            Myarray_i = ints;
+        }
+
+        public MyArray(params char[] chars)
+        {
+            Myarray_c = chars;
+        }
 
         public object this[int index]  //индексатор
         {
