b3078d8ee469f09c26c2042f8c0d1c3779395605
bruhzinga <48861240+bruhzinga@users.noreply.github.com>
2021-09-30 11:29:58 +0300

    DONE SHOULD PUSH TO MAIN BUT IDK HOW

--- LABA4/LABA4/myArray.cs
+++ LABA4/LABA4/myArray.cs
@@ -46,43 +46,163 @@ namespace LABA4
 
         public Owner _Owner = new Owner("DIMA", "BSTU");
         public Date _Date = new Date();
-        private int[] Myarray_i { get; set; }
+        public int[] myarray_i;
 
-        private char[] Myarray_c { get; set; }
+        public char[] myarray_c;
+
+        public MyArray()
+        {
+        }
 
         public MyArray(params int[] ints)
         {
-            Myarray_i = ints;
+            myarray_i = ints;
         }
 
         public MyArray(params char[] chars)
         {
-            Myarray_c = chars;
+            myarray_c = chars;
         }
 
         public object this[int index]  //индексатор
         {
             get
             {
-                if (Myarray_i == null)
+                if (myarray_i == null)
                 {
-                    return Myarray_c[index];
+                    return myarray_c[index];
                 }
                 else
                 {
-                    return Myarray_i[index];
+                    return myarray_i[index];
                 }
             }
 
             set
             {
-                if (Myarray_i == null)
+                if (myarray_i == null)
                 {
-                    value = Myarray_c[index];
+                    value = myarray_c[index];
                 }
                 else
                 {
-                    value = Myarray_i[index];
+                    value = myarray_i[index];
+                }
+            }
+        }
+
+        public static MyArray operator -(MyArray first, MyArray second)
+        {
+            MyArray newArray = new MyArray();
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                newArray.myarray_i[i] = first.myarray_i[i] - second.myarray_i[i];
+            }
+            return newArray;
+        }
+
+        public static MyArray operator +(MyArray first, MyArray second)
+        {
+            MyArray newArray = new MyArray();
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                newArray.myarray_i[i] = first.myarray_i[i] + second.myarray_i[i];
+            }
+            return newArray;
+        }
+
+        public static bool operator !=(MyArray first, MyArray second)
+        {
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                if (first.myarray_i[i] != second.myarray_i[i])
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(MyArray first, MyArray second)
+        {
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                if (first.myarray_i[i] == second.myarray_i[i])
+                    return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            if (myarray_c == null)
+                return string.Join(" ", myarray_i);
+            else
+            {
+                return string.Join(" ", myarray_c);
+            }
+        }
+
+        public static bool operator >(MyArray first, int second)
+        {
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                if (first.myarray_i[i] == second)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator <(MyArray first, int second)
+        {
+            for (int i = 0; i < first.myarray_i.Length; i++)
+            {
+                if (first.myarray_i[i] == second)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    internal static class StatisticOperation
+    {
+        public static int sum(MyArray inc)
+        {
+            return inc.myarray_i.Sum();
+        }
+
+        public static int diff(MyArray inc)
+        {
+            return (inc.myarray_i.Max() - inc.myarray_i.Min());
+        }
+
+        public static int count(MyArray inc)
+        {
+            return inc.myarray_i.Length;
+        }
+
+        public static void delGlas(this MyArray inc)
+        {
+            char[] glas = new char[] { 'а', 'и', 'е', 'ё', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+            foreach (char bukv in inc.myarray_c)
+            {
+                if (glas.Contains(bukv))
+                {
+                    inc.myarray_c = inc.myarray_c.Where(val => val != bukv).ToArray();
+                }
+            }
+        }
+
+        public static void del5(this MyArray inc)
+        {
+            int[] first = new int[6];
+            for (int i = 0; i < 5; i++)
+            {
+                first[i] = inc.myarray_i[i];
+            }
+            foreach (int ind in inc.myarray_i)
+            {
+                if (first.Contains(ind))
+                {
+                    inc.myarray_i = inc.myarray_i.Where(val => val != ind).ToArray();
                 }
             }
         }
