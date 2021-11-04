2a54a44dad5875355b7fa6cc6aebc38ff0e118be
bruhzinga <48861240+bruhzinga@users.noreply.github.com>
2021-11-04 22:59:34 +0300

    yonked some code for IDictionary

---
+++ LABA10/LABA10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LABA10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var Harry_Potter = new Book(2);
+            Harry_Potter.Add(12, "NICE");
+            foreach (DictionaryEntry de in Harry_Potter)
+            {
+                Console.WriteLine(Convert.ToString(de.Key) + de.Value);
+            }
+        }
+    }
+}
